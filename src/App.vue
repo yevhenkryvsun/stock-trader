d9c6e6c9102bf62a0c64d63c0510027c3d86860e
yevsun <yevhen,kryvsun@gmail.com>
2019-02-19 17:01:16 +0200

    Init store and add Stocks into it

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,10 @@
     export default {
         components: {
           appHeader: Header
-        }
+        },
+      created() {
+          this.$store.dispatch('initStocks');
+      }
     }
 </script>
 
